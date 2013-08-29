baddef2adecd999f40d2b0aa85924a316783cad0
Nikolai Ivanov <nikolai@threadsuite.com>
2013-08-29 16:31:08 +0300

    New app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsEmberApp::Application.config.secret_token = '0157c315e9dfd22049974fe4afb62f5d1f7dcfb07388aef3401418af5900a29066bdb5bfddc8ce50bd1479a92b5a09cb4f13be317df90a732e96e18fb3ebd437'
